834bf019ffec6307704d2ba0731f509d23ae3dda
Eliana <ewanginspiration@gmail.com>
2023-07-24 11:25:49 -0400

    Initial Commit

---
+++ Clockwork/ClockworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockworkApp.swift
+//  Clockwork
+//
+//  Created by Eliana Wang on 7/24/23.
+//
+
+import SwiftUI
+
+@main
+struct ClockworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClockworkUITests/ClockworkUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ClockworkUITestsLaunchTests.swift
+//  ClockworkUITests
+//
+//  Created by Eliana Wang on 7/24/23.
+//
+
+import XCTest
+
+final class ClockworkUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
