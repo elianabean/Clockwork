2a4a54aafa3f1d3b6c642b74cebce033318f8828
Eliana <ewanginspiration@gmail.com>
2023-10-09 13:04:17 -0400

    added text and buttons to login page

--- Clockwork/Login1View.swift
+++ Clockwork/Login1View.swift
@@ -24,13 +24,52 @@ struct Login1View: View {
                             .ignoresSafeArea()
                             .frame(width: geo.size.width, height: geo.size.height * 0.60, alignment: .center)
                         
-                        Text("Clockwork")
-                            .font(.largeTitle)
+                        Text("**Clockwork**")
+                            .font(.system(size: 70))
                             .foregroundColor(.white)
                         
                     }
                     
                     Spacer()
+                    
+                    Text("An app designed to help you stick to your routines!")
+                        .font(.system(size: 24))
+                        .foregroundColor(.black)
+                        .padding(EdgeInsets(top: 0, leading: 40, bottom: 0, trailing: 40))
+                        .multilineTextAlignment(.center)
+                    
+                    Spacer()
+                    
+                    HStack{
+                        Spacer()
+                        Button {
+                        } label: {
+                            Text("Log in")
+                                .font(.system(size: 20))
+                                .padding(EdgeInsets(top: 20, leading: 30, bottom: 20, trailing: 30))
+                        }
+                            
+                            .foregroundColor(.white)
+                            .background(.gray)
+                            .cornerRadius(10)
+                        
+                        Spacer()
+                        
+                        Button {
+                        } label: {
+                            Text("Sign Up")
+                                .font(.system(size: 20))
+                                .padding(EdgeInsets(top: 20, leading: 30, bottom: 20, trailing: 30))
+                        }
+                            .foregroundColor(.white)
+                            .background(.gray)
+                            .cornerRadius(10)
+                        
+                        Spacer()
+                    }
+                    
+                    Spacer()
+                    
                 }
                 
                 
